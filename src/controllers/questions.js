2c42bf12c670cf5691652e65e0f5fb91a7893569
mayangi aretha <myaretha2620@gmail.com>
2022-07-24 21:10:50 +0300

    add the questions controller

---
+++ src/controllers/questions.js
@@ -0,0 +1,11 @@
+import { questions } from '../db/db';
+
+class QuestionController {
+  static async getAllQuestions(req, res) {
+    try {
+      return res.status(200).json(questions);
+    } catch (error) {
+      console.log(e.message);
+    }
+  }
+}
