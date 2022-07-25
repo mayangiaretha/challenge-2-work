0f80ac9b74f18847d9bbd13f390b89a23280fed4
mayangi aretha <myaretha2620@gmail.com>
2022-07-26 00:01:47 +0300

    fix merge conflicts

--- src/controllers/questions.js
+++ src/controllers/questions.js
@@ -73,37 +73,14 @@ class QuestionController {
       const deletedQuestion = questions.findIndex(
         (question) => question.id === id
       );
-      if (!deletedQuestion)
+      if (!deletedQuestion) {
         return res.status(400).json({ message: 'Question not found' });
-      if (deletedQuestion) {
-        questions.splice(deletedQuestion, 1);
-        return res.status(201).json({ message: 'Question deleted' });
       }
+      questions.splice(deletedQuestion, 1);
+      return res.status(201).json({ message: 'question deleted ' });
     } catch (error) {
       console.log(error.message);
     }
-  }
-  static createAQuestion(req, res) {
-    const { title, description } = req.body;
-
-    const createAQuestion = { id: uuidv4(), title, description };
-    questions.push(createAQuestion);
-
-    res.status(201).json({
-      question: createAQuestion,
-      message: 'the question has been created',
-    });
-  }
-  static getAQuestion(req, res) {
-    const { id } = req.params;
-
-    const foundQuestion = questions.find((question) => question.id === id);
-    if (!foundQuestion) {
-      return res
-        .status(200)
-        .json({ error: 'question does not exist please check id' });
-    }
-    res.send(foundQuestion);
   }
 }
 
