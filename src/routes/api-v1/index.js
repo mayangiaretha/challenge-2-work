3cc7dc0281b69ad91088ca8b1950d054714381e3
mayangi aretha <myaretha2620@gmail.com>
2022-07-27 12:36:04 +0300

    added answers route

--- src/routes/api-v1/index.js
+++ src/routes/api-v1/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import questionsRoute from './questions.route';
+import answersRoute from './answers.route';
 
 const routes = Router();
 
 routes.use('/questions', questionsRoute);
+routes.use('/answers', answersRoute);
 
 export default routes;
